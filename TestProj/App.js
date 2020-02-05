7990bef24532b9ff4ca1d67a8d2b1ce4f82fdef1
Yash Maniyar <yashsmaniyar@DNa1d6784.SUNet>
2020-02-04 19:33:20 -0800

    added MakeCommunity component

--- TestProj/App.js
+++ TestProj/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, 
+	StyleSheet, 
+	Text, View, 
+	TextInput, 
+	Button,
+	Picker,
+	Alert } from 'react-native';
+import { Dropdown } from 'react-native-material-dropdown';
+import MakeCommunity from './MakeCommunity.js';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
 
+
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Stanford Safety.</Text>
-        <Text style={styles.instructions}>This branch is for working on initial features.</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <MakeCommunity />
       </View>
     );
   }
@@ -23,12 +30,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'white',
   },
-  welcome: {
+  page: {
     fontSize: 20,
+    flex: 1, 
+    flexDirection: 'column',
     textAlign: 'center',
-    margin: 10,
+    marginTop: 80,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 80
+  },
+  textInput: {
+  	fontSize: 20
+  },
+  text: {
+  	fontSize: 20
   },
   instructions: {
     textAlign: 'center',
