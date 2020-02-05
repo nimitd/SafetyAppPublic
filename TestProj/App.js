270af2ce553ba5fc63d52381dbf399c8119396cd
Gita Krishna <gitakrishna@DN51utgq.SUNet>
2020-02-04 19:22:03 -0800

    hola

--- TestProj/App.js
+++ TestProj/App.js
@@ -1,38 +1,169 @@
+
+// cite: https://www.bootdey.com/react-native-snippet/9/Login-form-ui-example
+
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button, 
+  TouchableHighlight,
+  Alert,
+  Picker
+} from 'react-native';
+import CreateOrJoin from './Components/CreateOrJoin';
+import { Dropdown } from 'react-native-material-dropdown';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+export default class Register extends Component {
 
-export default class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    state = {
+      name   : '',
+      suid: '',
+      dorm: '',
+      displayButtons: false,
+    }
+  }
+
+  buttonListener = () => {
+  	this.setState({
+  		displayButtons: !this.state.displayButtons
+  	});
+
+
+  }
+
+  state = {dorm: ''}
+   updateDorm = (dorm) => {
+      this.setState({ dorm: dorm })
+   }
+
+render() {
+
+	if (this.state.displayButtons) { 
+		return <CreateOrJoin/>
+
+	} else {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>See if you got this change.</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+    <View style = {styles.container}>
+    <View style = {styles.textboxcontainers}>
+      <Text style = {styles.header}> Enter your information below to get started. </Text>
+	    <View style={styles.inputContainer}>
+	      <TextInput style={styles.inputs}
+	          placeholder="Full name"
+	          onChangeText={(name) => this.setState({name})}/>
+	    </View>
+
+
+	    <View style={styles.inputContainer}>
+          <TextInput style={styles.inputs}
+              placeholder="SUID (eg: gitakris)"
+              onChangeText={(suid) => this.setState({suid})}/>
+        </View>
       </View>
+
+       <View style = {styles.dropdown}>
+      <Dropdown
+		        label='Choose your residence'
+		        data={[
+		        {value: 'Mars'},
+		        {value: '680'},
+		       	{value: 'Xanadu'},
+		        {value: 'Casa'},
+		        {value: 'Bob'},
+		        {value: 'Storey'},
+		        {value: 'Grove'},
+		        {value: 'Slav'},
+		        {value: 'Haus Mitt'},
+		        {value: 'Phi Sig'},
+		        {value: 'Kairos'},
+		        {value: 'EBF'},
+		        {value: 'Synergy'},
+		       	{value: 'Durand'},
+
+		        ]}
+		      />
+		</View>
+		<View style = {styles.buttonSpaceContainer}>
+		<TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.buttonListener()}>
+          <Text style={styles.loginText}>Register</Text>
+        </TouchableHighlight>
+        </View>
+
+
+
+     </View>
     );
+}
   }
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     justifyContent: 'center',
+    backgroundColor: '#DCDCDC',
+  },
+  buttonSpaceContainer: {
+  	flex: 0.5, 
+  	justifyContent: 'center',
+  	alignItems: 'center',
+  },
+  textboxcontainers: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  header: {
+  	fontSize: 20,
+    textAlign: 'center',
+    margin: 30,
+	},
+  inputContainer: {
+      borderBottomColor: '#F5FCFF',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center',
+  },
+  inputs:{
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#FFFFFF',
+      flex:1,
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+  },
+  loginButton: {
+    backgroundColor: "maroon",
+  },
+  loginText: {
+    color: 'white',
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  dropdown: {
+  	flexDirection: 'column',
+  	justifyContent: 'center',
+  	marginLeft: 20,
+  	marginRight: 20,
   },
 });
+
+
+
+
+
+
+
